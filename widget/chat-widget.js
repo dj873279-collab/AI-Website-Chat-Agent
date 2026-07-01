c42266377138b6f120a6615ada808af9f2081943
Darshil Joshi <dj873279@gmail.com>
2026-07-01 23:42:58 +0530

    Added embeddable widget

---
+++ widget/chat-widget.js
@@ -0,0 +1,41 @@
+window.ChatWidget = {
+  init: function (config) {
+
+    const button = document.createElement("button");
+
+    button.innerHTML = "💬";
+
+    button.style.position = "fixed";
+    button.style.bottom = "20px";
+    button.style.right = "20px";
+    button.style.width = "60px";
+    button.style.height = "60px";
+    button.style.borderRadius = "50%";
+    button.style.cursor = "pointer";
+    button.style.zIndex = "9999";
+
+    document.body.appendChild(button);
+
+    const iframe = document.createElement("iframe");
+
+    iframe.src = "http://localhost:5173";
+
+    iframe.style.position = "fixed";
+    iframe.style.bottom = "90px";
+    iframe.style.right = "20px";
+    iframe.style.width = "400px";
+    iframe.style.height = "600px";
+    iframe.style.border = "none";
+    iframe.style.display = "none";
+    iframe.style.zIndex = "9999";
+
+    document.body.appendChild(iframe);
+
+    button.onclick = () => {
+      iframe.style.display =
+        iframe.style.display === "none"
+          ? "block"
+          : "none";
+    };
+  }
+};
